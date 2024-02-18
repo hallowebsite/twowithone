f5d9ab7dda53a5e0eb899502eaa0391b3e59c160
Piotr Halasiewicz <piotr.halasiewicz@scout24.com>
2024-02-19 00:09:35 +0100

    Fonts

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,6 +5,10 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ["Helvetica", "sans-serif"],
+      serif: ["Georgia", "serif"],
+    },
   },
   plugins: [
     require("@tailwindcss/forms"),
