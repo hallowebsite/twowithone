e2e982340594344de0a3bb6498c12f48187b4623
Piotr Halasiewicz <piotr.halasiewicz@scout24.com>
2024-02-23 23:36:40 +0100

    Lqip modern

---
+++ src/lib/image.ts
@@ -0,0 +1,27 @@
+// src/lib/image.ts
+import type { LocalImageProps } from "astro:assets";
+import fs from "node:fs/promises";
+
+export const getImageAsBuffer = async (src: LocalImageProps["src"]) => {
+  const ASSETS_DIR = "/src/images";
+
+  const imageMetadata = await src;
+
+  return await fs.readFile(
+    new URL(
+      import.meta.env.PROD
+        ? [
+            "../../dist",
+            // @ts-expect-error – 'Property 'src' does not exist on type'
+            imageMetadata?.src,
+          ].join("")
+        : [
+            "../..",
+            ASSETS_DIR,
+            // @ts-expect-error – 'Property 'src' does not exist on type'
+            imageMetadata?.default.src.split("?orig")[0].split(ASSETS_DIR)[1],
+          ].join(""),
+      import.meta.url,
+    ),
+  );
+};
