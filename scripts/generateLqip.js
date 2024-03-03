5c1f222d8c2094ce4fe44f4ba3aef5cb60b2d451
Piotr Halasiewicz <piotr.halasiewicz@scout24.com>
2024-03-04 00:09:08 +0100

    Adds lqip

---
+++ scripts/generateLqip.js
@@ -0,0 +1,25 @@
+import fs from "node:fs/promises";
+import path from "node:path";
+import lqip from "lqip-modern";
+
+const inputDirectory = "src/images/"; // Change this to your input directory
+const outputJsonPath = "./images.json"; // Replace with your desired output JSON file path
+
+const output = {};
+
+const files = await fs.readdir(inputDirectory);
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+async function processFiles() {
+  for (const fileName of files) {
+    const filePath = path.join(inputDirectory, fileName);
+    const result = await lqip(filePath);
+    output[fileName.split(".")[0]] = result.metadata.dataURIBase64;
+  }
+  console.log(output);
+  await fs.writeFile(outputJsonPath, JSON.stringify(output, null, 2));
+}
+try {
+  await processFiles();
+} catch (error) {
+  console.error("Error generating lqip", error);
+}
