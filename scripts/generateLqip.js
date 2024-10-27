0d8b02cd7d13899de9fd2e00f187d20400bec9ac
Piotr Halasiewicz <piotr.halasiewicz@scout24.com>
2024-10-28 00:11:06 +0100

    Try/catch for lqip

--- scripts/generateLqip.js
+++ scripts/generateLqip.js
@@ -11,9 +11,13 @@ const files = await fs.readdir(inputDirectory);
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 async function processFiles() {
   for (const fileName of files) {
-    const filePath = path.join(inputDirectory, fileName);
-    const result = await lqip(filePath);
-    output[fileName.split(".")[0]] = result.metadata.dataURIBase64;
+    try {
+      const filePath = path.join(inputDirectory, fileName);
+      const result = await lqip(filePath);
+      output[fileName.split(".")[0]] = result.metadata.dataURIBase64;
+    } catch (error) {
+      console.error("Error processing file:", fileName, error);
+    }
   }
   console.log(output);
   await fs.writeFile(outputJsonPath, JSON.stringify(output, null, 2));
