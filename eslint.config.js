c388d510c07c2b6a22b555bf4538d58351bd56f9
Piotr Halasiewicz <piotr.halasiewicz@scout24.com>
2025-04-13 21:44:42 +0200

    Changes to eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,21 @@
 import globals from "globals";
+import { defineConfig, globalIgnores } from "eslint/config";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import eslintPluginAstro from "eslint-plugin-astro";
 
-export default [
-  { files: ["**/*.{js,mjs,cjs,ts}"] },
+export default defineConfig([
+  globalIgnores(["dist/**"]),
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+  },
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "warn", // Changed from error to warning
+    },
+  },
   ...eslintPluginAstro.configs.recommended,
-];
+]);
